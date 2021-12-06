a14a5f433d84f7588a928b8dc118e2a0298cc058
Sebastian Taras <sebastian.taras@ericsson.com>
2021-12-06 13:15:18 +0100

    cringe

--- index.js
+++ index.js
@@ -6,39 +6,45 @@ const app = express();
 app.use(cors());
 
 const connString =
-	"mongodb+srv://admin:admin@cluster.nkyj5.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+  "mongodb+srv://admin:admin@cluster.nkyj5.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
 
 mongoose.connect(connString);
 mongoose.connection.once("open", () => {
-	console.log("connected bro");
+  console.log("connected bro");
 });
-app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-var Schema = mongoose.Schema;
 
-var userSchema = new Schema({
-	id: String,
-	name: String,
-	email: String,
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+  id: String,
+  name: String,
+  email: String,
 });
 
-var User = mongoose.model("user", userSchema);
+const User = mongoose.model("user", userSchema);
 
 app.listen(5000, () => {
-	console.log("listenign bro");
+  console.log("listenign bro");
 });
 
 app.get("/", (req, res) => {
-	res.send("Hello wlrd");
+  res.send("Hello wlrd");
 });
 
-app.post("/register", (req, res) => {
-	console.log(req.body);
-	const user = new User({
-		id: req.body.id.toString(),
-		name: req.body.name,
-		email: req.body.email,
-	});
-	user.save();
-	res.send("ok");
+app.post("/register", async (req, res) => {
+  console.log(req.body);
+  const user = new User({
+    id: req.body.id.toString(),
+    name: req.body.name,
+    email: req.body.email,
+  });
+  const userm = await User.findOne({ id: user.id });
+  console.log(userm);
+  if (userm) {
+    res.send("exists");
+  } else {
+    user.save();
+    res.send("ok");
+  }
 });
